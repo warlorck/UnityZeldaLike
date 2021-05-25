774f58e41e621458a71156f4a02f762baa0de5ea
Nicolas Salaün <nicolas.salaun19@gmail.com>
2021-05-25 22:22:13 +0200

    feat: part 3

---
+++ Assets/Scripts/Player/PlayerAnimate.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(PlayerMovements))]
+public class PlayerAnimate : MonoBehaviour
+{
+    public float walkDelta;
+    Animator animator;
+    SpriteRenderer spriteRenderer;
+    PlayerMovements playerMovements;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        playerMovements = GetComponent<PlayerMovements>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool isWalking = playerMovements.moveInputs.magnitude > walkDelta;
+        animator.SetBool("IsWalking", false);
+        animator.SetFloat("MoveX", playerMovements.moveInputs.x);
+        animator.SetFloat("MoveY", playerMovements.moveInputs.y);
+        spriteRenderer.flipX = playerMovements.moveInputs.x < 0;
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovements.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovements : MonoBehaviour
+{
+    public float speed;
+    [HideInInspector]
+    public Vector2 moveInputs;
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        moveInputs = new Vector2(x, y);
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 target = rb.position + moveInputs * speed * Time.deltaTime;
+        rb.MovePosition(target);
+    }
+}
